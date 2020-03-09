5a19cf1ee58347fa7b77155b58240607d98c1dd3
Ike Mawira <mawiraike@gmail.com>
2020-03-10 01:02:48 +0300

    Initial commit

---
+++ test/clojure_mpesa_wrapper/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-mpesa-wrapper.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-mpesa-wrapper.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
