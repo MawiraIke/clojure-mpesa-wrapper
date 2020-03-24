17287ff14a5cf7c19a444d3fb317310db4a13eae
Ike Mawira <mawiraike@gmail.com>
2020-03-24 23:39:04 +0300

    Disable some tests for now, to update them later
    Signed-off-by: Ike Mawira <mawiraike@gmail.com>

--- test/clojure_mpesa_wrapper/core_test.clj
+++ test/clojure_mpesa_wrapper/core_test.clj
@@ -7,8 +7,8 @@
     (is (= (encode (str "174379" "MTc0Mzc5YmZiMjc5ZjlhYTliZGJjZjE1OGU5N2RkNzFhNDY3Y2QyZTBjODkzMDU5YjEwZjc4ZTZiNzJhZGExZWQyYzkxOTIwMTgwNDA5MDkzMDAy" "20180409093002"))
            "MTc0Mzc5TVRjME16YzVZbVppTWpjNVpqbGhZVGxpWkdKalpqRTFPR1U1TjJSa056RmhORFkzWTJReVpUQmpPRGt6TURVNVlqRXdaamM0WlRaaU56SmhaR0V4WldReVl6a3hPVEl3TVRnd05EQTVNRGt6TURBeTIwMTgwNDA5MDkzMDAy"))))
 
-;; Requires a key and a secret to pass
-(deftest authentication-test
-  (testing "Authentication"
-    (is (= (type (auth "<Enter key as a String" "<Enter secret as a String>"))
-           clojure.lang.PersistentArrayMap))))
+;; Requires a key and a secret to pass todo write tests
+;;(deftest authentication-test
+;;  (testing "Authentication"
+;;    (is (= (type (auth "<Enter key as a String" "<Enter secret as a String>"))
+;;           clojure.lang.PersistentArrayMap))))
